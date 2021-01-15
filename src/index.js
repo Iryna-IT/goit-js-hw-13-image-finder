09d73854d2598b99aeb1211284df72dd70109117
Iryna Redka <redkaiv@gmail.com>
2021-01-15 19:16:32 +0200

    added scroll

--- src/index.js
+++ src/index.js
@@ -49,20 +49,34 @@ function renderGallery(event) {
     // const limit = data.totalHits;
     // console.log(data);
       
-      if (data.total >= 1) {
-        galleryRef.insertAdjacentHTML('beforeend', gallery);
-        const galleryItemRef = document.querySelectorAll(".gallery");
-
-        Array.prototype.map.call(galleryItemRef,
-          function (obj) {
-            obj.insertAdjacentHTML('beforeend', photoCard);
-          
-            if (data.total > 12) {
-              loadButtonRef.style.display = 'inline';
-            }
-  })
+    if (data.total >= 1) {
+      galleryRef.insertAdjacentHTML('beforeend', gallery);
+      const galleryListRef = document.querySelectorAll(".gallery");
+      const countGalleryList = galleryListRef.length;
+      const targetGalleryListRef = galleryListRef.[countGalleryList - 1];
+      // console.log(galleryListRef);
+      // console.log(countGalleryList);
+      // console.log(galleryItemRef);
+      targetGalleryListRef.insertAdjacentHTML('beforeend', photoCard);
+       
+      // function zoomImg(event) {
+      //   const targetImg = event.target;
+      //   const newSrc = targetImg.dataset.url;
         
-      } else {
+      // }
+
+      // const imgRef = document.querySelectorAll("img");
+      // Array.prototype.map.call(imgRef,
+      //   function (obj) {
+      //     obj.addEventListener('click', zoomImg);
+      //   });
+          
+          if (data.total > 12) {
+            loadButtonRef.style.display = 'inline';
+          }
+    }
+        
+       else {
         inputRef.value = "";
       error ({
   text: "Nothing found. Please, enter a correct query"
@@ -72,11 +86,55 @@ function renderGallery(event) {
   }).catch(error => console.log(error))
 }
 
+// let scrollHeight = document.querySelector(".gallery").offsetHeight; 
+// console.log(scrollHeight)
+
+
 function loadImages(event) {
-  pageNumber += 1;
+pageNumber += 1;
   searchQuery = inputRef.value;
-  // renderGallery(event);
-  // console.log(searchQuery);
+  renderGallery(event);
+
+
+  function currentYPosition() {
+    if (self.pageYOffset) return self.pageYOffset;
+    return 0;
+}
+
+  function elmYPosition() {
+    let y = loadButtonRef.offsetTop - 20;
+    let node = document.querySelector(".gallery");
+    console.log(y);
+    while (node.offsetParent && node.offsetParent != document.body) {
+      node = node.offsetParent;
+      y += node.offsetTop;
+    } return y;
+}
+
+function smoothScroll() {
+    const startY = currentYPosition();
+    const stopY = elmYPosition();
+    const distance = stopY > startY ? stopY - startY : startY - stopY;
+    if (distance < 100) {
+        scrollTo(0, stopY); return;
+    }
+    var speed = Math.round(distance / 100);
+    if (speed >= 20) speed = 20;
+    var step = Math.round(distance / 25);
+    var leapY = stopY > startY ? startY + step : startY - step;
+    var timer = 0;
+    if (stopY > startY) {
+        for ( let i=startY; i<stopY; i+=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+        } return;
+    }
+    for ( let i=startY; i>stopY; i-=step ) {
+        setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+        leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+    }
+}
+  smoothScroll();
 }
 
 
