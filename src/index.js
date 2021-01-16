845ea38b6f13426f25f91eb3086f9780afd8f3c2
Iryna Redka <redkaiv@gmail.com>
2021-01-16 22:04:42 +0200

    axios

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './styles.css';
-// import './js/apiService.js';
 
 import fetchGallery from './js/apiService.js';
 import galleryTemplate from './templates/gallery.hbs';
@@ -26,32 +25,25 @@ let pageNumber = 1;
 let searchQuery = inputRef.value;
 
 // ==============СЛУШАТЕЛИ СОБЫТИЙ====================
-inputRef.addEventListener('input', debounce(initializeRenderGallery, 500));
+inputRef.addEventListener('input', debounce(renewRequest, 500));
 loadButtonRef.addEventListener('click', loadImages);
 
-
 // =========ФУНКЦИЯ СОЗДАНИЯ ГАЛЛЕРЕИИ==============
-function initializeRenderGallery(event) {
+function renewRequest(event) {
 searchQuery = event.target.value;
   if (searchQuery !== "") {
     galleryRef.innerHTML = "";
+    loadButtonRef.style.display = 'none';
   }
   renderGallery(searchQuery);
 }
 
-// function renderGallery(event) {
-//   searchQuery = event.target.value;
-//   if (searchQuery !== "") {
-//     galleryRef.innerHTML = "";
-//   }
-
+// =========функция создания галлереии ==============
 function renderGallery(searchQuery) {
   // ================Получение данных для заполнения шаблона галереии============== 
   fetchGallery(key, pageNumber, searchQuery).then(data => {
     const photoCard = photoCardTemplate(data.hits);
     const gallery = galleryTemplate(data.hits);
-    // const limit = data.totalHits;
-    // console.log(data);
       
     // ================Рендер разметки галереи============== 
     if (data.total >= 1) {
@@ -103,7 +95,8 @@ pageNumber += 1;
 // ============Функция определения текущего положения элемента на странице============== 
   function elmYPosition() {
     let y = loadButtonRef.offsetTop - 20;
-    let node = document.querySelector(".gallery");
+    let node = document.querySelectorAll(".gallery").[document.querySelectorAll(".gallery").length - 1];
+    // let node = document.querySelector(".gallery");
     while (node.offsetParent && node.offsetParent != document.body) {
       node = node.offsetParent;
       y += node.offsetTop;
@@ -118,22 +111,145 @@ function smoothScroll() {
     if (distance < 100) {
         scrollTo(0, stopY); return;
     }
-    var speed = Math.round(distance / 100);
+    const speed = Math.round(distance / 100);
     if (speed >= 20) speed = 20;
-    var step = Math.round(distance / 25);
-    var leapY = stopY > startY ? startY + step : startY - step;
-    var timer = 0;
+    const step = Math.round(distance / 25);
+    let leapY = stopY > startY ? startY + step : startY - step;
+    let timer = 0;
     if (stopY > startY) {
         for ( let i=startY; i<stopY; i+=step ) {
             setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
-            leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+            leapY += step; if (leapY > stopY) leapY = stopY; timer+=1;
         } return;
     }
     for ( let i=startY; i>stopY; i-=step ) {
         setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
-        leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+        leapY -= step; if (leapY < stopY) leapY = stopY; timer+=1;
     }
   }
   // ============Вызов функции скролла============== 
   smoothScroll();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// =============================================================
+// ==============РАБОЧАЯ ВЕРСИЯ ДЛЯ FETCH =====================
+// ============================================================
+// =========СОЗДАНИЕ ГАЛЛЕРЕИИ==============
+// =========Функция обновления запроса==============
+// function renewRequest(event) {
+// searchQuery = event.target.value;
+//   if (searchQuery !== "") {
+//     galleryRef.innerHTML = "";
+//   }
+//   renderGallery(searchQuery);
+// }
+// =========Функция создания галлереии==============
+// function renderGallery(searchQuery) {
+//   // ================Получение данных для заполнения шаблона галереии============== 
+//   fetchGallery(key, pageNumber, searchQuery).then(data => {
+//     const photoCard = photoCardTemplate(data.hits);
+//     const gallery = galleryTemplate(data.hits);
+//     // const limit = data.totalHits;
+//     console.log(data);
+      
+//     // ================Рендер разметки галереи============== 
+//     if (data.total >= 1) {
+//       galleryRef.insertAdjacentHTML('beforeend', gallery);
+//       const galleryListRef = document.querySelectorAll(".gallery");
+//       const countGalleryList = galleryListRef.length;
+//       const targetGalleryListRef = galleryListRef.[countGalleryList - 1];
+//       targetGalleryListRef.insertAdjacentHTML('beforeend', photoCard);
+      
+//       // =======Открытие большого изображения по клику======
+//       galleryRef.onclick = (event) => {
+//         const src = event.target.dataset.url;
+//         const largeImg = basicLightbox.create(`<img src=${src} class="show">`);
+//         largeImg.show();
+//         // =======Закрытие большого изображения по клику======
+//         const closelargeImg = function () { largeImg.close() };
+//         const showImgRef = document.querySelector(".show");
+//         showImgRef.addEventListener('click', closelargeImg);
+//       };
+//       // ===========Добавление кнопки дозагрузки картинок==============  
+//       if (data.total > 12) {
+//         loadButtonRef.style.display = 'inline';
+//       }
+//       // ====================очистка запроса======================
+      
+//     }
+//     // ================Сообщение об ошибке============== 
+//     else {
+//       inputRef.value = "";
+//       error({
+//         text: "Nothing found. Please, enter a correct query"
+//       });
+//       return error;
+//     }
+//   }).catch(error => console.log(error));
+// }
+
+// // ================ФУНКЦИЯ ДОЗАГРУЗКИ ИЗОБРАЖЕНИЙ ПО СОБЫТИЮ И СКРОЛЛА============== 
+// function loadImages() {
+// pageNumber += 1;
+//   renderGallery(searchQuery);
+
+// // ================Функция определения текущего положения на странице============== 
+//   function currentYPosition() {
+//     if (self.pageYOffset) return self.pageYOffset;
+//     return 0;
+// }
+
+// // ============Функция определения текущего положения элемента на странице============== 
+//   function elmYPosition() {
+//     let y = loadButtonRef.offsetTop - 20;
+//     let node = document.querySelectorAll(".gallery").[document.querySelectorAll(".gallery").length - 1];
+//     // let node = document.querySelector(".gallery");
+//     while (node.offsetParent && node.offsetParent != document.body) {
+//       node = node.offsetParent;
+//       y += node.offsetTop;
+//     } return y;
+// }
+
+// // ============Функция плавного скролла============== 
+// function smoothScroll() {
+//     const startY = currentYPosition();
+//     const stopY = elmYPosition();
+//     const distance = stopY > startY ? stopY - startY : startY - stopY;
+//     if (distance < 100) {
+//         scrollTo(0, stopY); return;
+//     }
+//     const speed = Math.round(distance / 100);
+//     if (speed >= 20) speed = 20;
+//     const step = Math.round(distance / 25);
+//     let leapY = stopY > startY ? startY + step : startY - step;
+//     let timer = 0;
+//     if (stopY > startY) {
+//         for ( let i=startY; i<stopY; i+=step ) {
+//             setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+//             leapY += step; if (leapY > stopY) leapY = stopY; timer+=1;
+//         } return;
+//     }
+//     for ( let i=startY; i>stopY; i-=step ) {
+//         setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+//         leapY -= step; if (leapY < stopY) leapY = stopY; timer+=1;
+//     }
+//   }
+//   // ============Вызов функции скролла============== 
+//   smoothScroll();
+// }
+// ==========================================================================================
+// ==========================================================================================
+
+
+
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,12 +1,49 @@
-// import axios from 'axios';
+import { data } from 'autoprefixer';
+import axios from 'axios';
 
-function fetchGallery(key, pageNumber,searchQuery) {
+// ============синхронный axios =======================
+const fetchGallery = (key, pageNumber, searchQuery) =>
+    axios.get(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${pageNumber}&per_page=12&key=${key}`)
+        .then(response => response)
+        .then(dataSet => dataSet.data)
 
-   return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${pageNumber}&per_page=12&key=${key}`)
-       .then(response => response.json())
-        .catch(error => {
-            console.log(error);
-        });
-}
+
+// ============подробный синхронный axios =======================
+// const fetchGallery = (key, pageNumber, searchQuery) =>
+//     axios.get(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${pageNumber}&per_page=12&key=${key}`)
+//         .then(response => {
+//             console.log(response);
+//             return response;
+//         })
+//         .then(dataSet => {
+//             console.log(dataSet.data);
+//             return dataSet.data;
+//         })
+//     .then(data => {
+//     console.log(data.hits);
+// })
+
+
+// ============синхронный fetch =======================
+// function fetchGallery(key, pageNumber,searchQuery) {
+
+//    return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${pageNumber}&per_page=12&key=${key}`)
+//        .then(response => response.json())
+//         .catch(error => {
+//             throw error;
+//         });
+// }
+
+
+// ============асинхронный fetch=======================
+// async function fetchGallery(key, pageNumber, searchQuery) {
+//     try {
+//         const response = await fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${pageNumber}&per_page=12&key=${key}`);
+//         const images = response.json();
+//         return images;
+//     } catch (error) {
+//             throw error;
+//         };
+// }
 
 export default fetchGallery
